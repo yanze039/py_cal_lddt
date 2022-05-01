f00c111e7a9d8f5844ba0203c66ee3a46dcdf0ca
Yanze Wang <43460571+Dead-fisher@users.noreply.github.com>
2022-05-01 16:59:21 +0800

    Update cal_lddt.py

--- cal_lddt.py
+++ cal_lddt.py
@@ -23,7 +23,6 @@ def cal_lddt(ref, model, cutoff=15.):
     cutoff: unit - angstrom
     """
     residue_length = model.shape[0]
-    ref = ref[:residue_length,:residue_length]
     boolean_contact = ref<cutoff
     diff = model[boolean_contact] - ref[boolean_contact]
     contact_num1 = np.sum( np.abs(diff) < 0.5 ) - residue_length
@@ -34,11 +33,28 @@ def cal_lddt(ref, model, cutoff=15.):
     lddt = (contact_num1 + contact_num2 + contact_num3 + contact_num4) / (origin_contact * 4)
     return lddt
 
+def cal_residue_lddt(ref, model, cutoff=15.):
+    residue_length = model.shape[0]
+    boolean_contact = ref<cutoff
+    residue_lddt = np.zeros((residue_length,))
+    for resi in range(residue_length):
+        diff = model[resi][boolean_contact[resi]] - ref[resi][boolean_contact[resi]]
+        contact_num1 = np.sum( np.abs(diff) < 0.5 ) - 1
+        contact_num2 = np.sum( np.abs(diff) < 1.  ) - 1
+        contact_num3 = np.sum( np.abs(diff) < 2.  ) - 1
+        contact_num4 = np.sum( np.abs(diff) < 4.  ) - 1
+        origin_contact = np.sum( boolean_contact[resi] ) - 1
+        lddt = (contact_num1 + contact_num2 + contact_num3 + contact_num4) / (origin_contact * 4)
+        residue_lddt[resi] = lddt
+    return residue_lddt
+
 def main(args):
     native_distogram = get_distogram(args.native)
     model_distogram = get_distogram(args.model)
-    lddt = cal_lddt(native_distogram, model_distogram, cutoff=15.)
-    print(lddt)
+    lddt = cal_lddt(native_distogram, model_distogram, cutoff=args.cutoff)
+    per_resi_lddt= cal_residue_lddt(ref, model, cutoff=args.cutoff)
+    print("Global lDDT:", lddt)
+    print("Per-residue lDDT:\n", lddt)
     return lddt
 
 
